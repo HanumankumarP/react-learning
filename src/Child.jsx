66fec6189c433756684a6e53d440523e082f21b2
fournotfourerror <fournotfourerror@gmail.com>
2023-05-11 21:03:07 +0530

    react-hook-form practice

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,15 +1,38 @@
-import {useState} from 'react'
-export default function Child({changeNameValue}){
-    const [name,setName]=useState({name:'',age:28})
+// import {useState} from 'react';
+import {useForm} from 'react-hook-form';
 
-    const updateName=(nameParam)=>{
-        setName(prevState=>({...prevState,name:nameParam.target.value}))
+const Child=()=>{
+    
+
+    const {
+        register,
+        handleSubmit,
+        formState:{errors}
+    } = useForm();
+
+    const submitData=(e)=>{
+        // e.preventDefault();
+        
+        console.log(e)
     }
-    return (
-        <form>
-            <input type="text" value={name.name} onChange={e=>updateName(e)}/> <br/>
-            <button onClick={changeNameValue(name.name)}> Update Name </button>
-        </form>
-    )
 
+    // const data1=false;
+    // const data2=true;
+
+    // data1===false && data2===true ? console.log('Success') : console.log('Fail')
+
+    return(
+        <>
+        <h2> Sample Form</h2>
+        <form onSubmit={handleSubmit(submitData)}>
+            <input type="text" placeholder="Name" {...register('name')} /> <br />
+            <input type="email" placeholder="Email" {...register('email',{required:true, pattern: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ })} /> <br />
+            {errors.email && errors.email.type==='required' && <label> Email is required </label>} 
+            {errors.email && errors.email.type==='pattern' && <label> Enter valid email </label>}
+            <button type="submit"> Submit form</button>
+        </form>
+        </>
+    )
 }
+
+export default Child
--- src/SampleComponent.jsx
+++ src/SampleComponent.jsx
@@ -7,9 +7,9 @@ export default function Praneeth(){
         age:28
     })
 
-    const increaseAge=()=>{
-       setData(prevState=>({...prevState,age:prevState.age+1})) 
-    }
+    // const increaseAge=()=>{
+    //    setData(prevState=>({...prevState,age:prevState.age+1})) 
+    // }
 
     const changeName=(nameArg)=>{
         setData(prevState=>({...prevState,name:nameArg}))
@@ -18,8 +18,8 @@ export default function Praneeth(){
     const {name,age}=data
     return(
         <>
-        <h2> {name} is {age} years old</h2>
-        <button onClick={()=>increaseAge()}> Increase age </button>
+        {/* <h2> {name} is {age} years old</h2> */}
+        {/* <button onClick={()=>increaseAge()}> Increase age </button> */}
         <Child changeNameValue={changeName}/>
         </>
     )
